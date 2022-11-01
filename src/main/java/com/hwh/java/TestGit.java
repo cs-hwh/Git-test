22c6ef7b90386640560f6fcc125a728a30723df0
hwh <hwh@qq.com>
2022-11-01 17:18:29 +0800

    IDEA first commit

---
+++ src/main/java/com/hwh/java/TestGit.java
@@ -0,0 +1,11 @@
+package com.hwh.java;
+
+/**
+ * @author hwh
+ * @create 2022-11-01 17:13
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
