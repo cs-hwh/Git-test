5b0d30bf3d744bb1edf662c7f6e6328e6bdfcc1f
hwh <hwh@qq.com>
2022-11-01 17:52:17 +0800

    master commit 1

--- src/main/java/com/hwh/java/TestGit.java
+++ src/main/java/com/hwh/java/TestGit.java
@@ -9,5 +9,7 @@ public class TestGit {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("master 1");
+
     }
 }
